b75d3d87b459557ab7f0c1c7ec9c7e87ea96ab3c
鲁思晨 <2399984023@qq.com>
2021-10-23 22:14:47 +0800

    123

--- src/hello.c
+++ src/hello.c
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-10 21:03:51
+ * @LastEditTime: 2021-10-23 22:14:12
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \lusichen\src\hello.c
+ */
 #include "hello.h"
 #include <stdio.h>
 
